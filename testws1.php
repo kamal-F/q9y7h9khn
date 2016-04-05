eedeec143049e9a53577d83177d22357b6bbec1b
Gamma Muhammad N_1144068 <gammamuhammad5@gmail.com>
2016-04-05 12:11:15 +0700

    Added files via upload

---
+++ testws1.php
@@ -0,0 +1,24 @@
+Tujuan webservice: http://localhost:81/ws1/ws2hitung.asmx/kali
+  <form action='http://localhost:81/ws1/ws2hitung.asmx/kali'
+  method="post" target="_blank"
+  <table>
+    <tr>
+    <td>prm1</td>
+    <td>
+    <input  type="text"
+    </td>
+    </tr>
+    <tr>
+    <td>prm2</td>
+    <td>
+<input  type="text" size="30" name="prm2">
+    </td>
+    </tr>
+    <tr>
+    <td></td>
+    <td align="right">
+    <input type="submit" value="Submit" class="button">
+    </td>
+    </tr>
+  </table>
+  </form>
